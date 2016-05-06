5f41f705c6c15ca5aceb468ba87afc057b307256
Satish Kalia <satish.kalia@gmail.com>
2016-05-06 12:40:41 +0200

    first commit

--- app/RomanNumeralParser.js
+++ app/RomanNumeralParser.js
@@ -1,3 +1,4 @@
+"use strict";
 
 var RomanNumeral = function(arabic, numeral) {
     this.arabic = arabic;
@@ -6,20 +7,50 @@ var RomanNumeral = function(arabic, numeral) {
 
 var romanNumerals = [
     new RomanNumeral(1, "I"),
+    new RomanNumeral(4, "IV"),
     new RomanNumeral(5, "V"),
+    new RomanNumeral(9, "IX"),
     new RomanNumeral(10, "X"),
+    new RomanNumeral(40, "XL"),
     new RomanNumeral(50, "L"),
+    new RomanNumeral(90, "XC"),
     new RomanNumeral(100, "C"),
+    new RomanNumeral(400, "CD"),
     new RomanNumeral(500, "D"),
+    new RomanNumeral(900, "CM"),
     new RomanNumeral(1000, "M")
 ];
 
 var RomanNumeralParser = function() {
     
     RomanNumeralParser.prototype.parse = function(arabic) {
-        var romanNumeral = romanNumerals.find (
-            function (r) { return r.arabic === arabic; }
-        );
-        return romanNumeral.numeral;
+        RomanNumeralParser.prototype.validateArabic(arabic);
+        
+        var parsedNumeral = "";
+        
+        var nextNumeral = RomanNumeralParser.prototype.getNextNumeral(arabic);
+        parsedNumeral += nextNumeral.numeral;
+        var remainder = arabic - nextNumeral.arabic;
+        if (remainder > 0) {
+            parsedNumeral += RomanNumeralParser.prototype.parse(remainder);
+        }
+        
+        return parsedNumeral;
     };
+    
+    RomanNumeralParser.prototype.getNextNumeral = function(arabic) {
+        return romanNumerals.
+            sort(function(lhs, rhs) { return rhs.arabic - lhs.arabic; }).
+            filter(function(romanNumeral) { return arabic >= romanNumeral.arabic; })[0];
+    }
+    
+    RomanNumeralParser.prototype.validateArabic = function(arabic) {
+        if (arabic < 1) {
+            throw new RangeError("Number should be greater than 0");
+        }
+        
+        if (arabic > 3000) {
+            throw new RangeError("Number should be less than 3001");
+        }
+    }
 };
--- test/spec/RomanNumeralParserSpec.js
+++ test/spec/RomanNumeralParserSpec.js
@@ -1,20 +1,61 @@
 "use strict";
 
-describe("Should parse arabic numbers to Roman Numerals", function() {
+var parseAndCheckResult = function(parseArabicNumber, expectedResult) {
+    var romanNumeralParser = new RomanNumeralParser();
+    var romanNumeral = romanNumeralParser.parse(parseArabicNumber);
+    return function () {
+        expect(romanNumeral).toEqual(expectedResult);
+    }
+};
 
-    var parseAndCheckResult = function(parseArabicNumber, expectedResult) {
+describe("Should reject arabic numbers outside acceptable range: ", function() {
+    it("should reject numbers less than 1", function() {
         var romanNumeralParser = new RomanNumeralParser();
-        var romanNumeral = romanNumeralParser.parse(parseArabicNumber);
-        return function () {
-            expect(romanNumeral).toEqual(expectedResult);
-        }
-    };
+        
+        expect(function() { romanNumeralParser.parse(0); }).
+            toThrow(new RangeError("Number should be greater than 0"));
+    });
+    
+    it("should reject numbers greater than 3000", function() {
+        var romanNumeralParser = new RomanNumeralParser();
+        
+        expect(function () { romanNumeralParser.parse(3001);}).
+            toThrow(new RangeError("Number should be less than 3001"));
+    });
+});
 
+describe("Should parse arabic numbers to single digit Roman Numerals: ", function() {
     it("should parse 1 to I", parseAndCheckResult(1, "I"));
     it("should parse 10 to X", parseAndCheckResult(10, "X"));
     it("should parse 50 to L", parseAndCheckResult(50, "L"));
     it("should parse 100 to C", parseAndCheckResult(100, "C"));
     it("should parse 500 to D", parseAndCheckResult(500, "D"));
     it("should parse 1000 to M", parseAndCheckResult(1000, "M"));
-    
+});
+
+describe("Should parse arabic numbers for repeating numerals: ", function() {
+    it("should parse 2 to II", parseAndCheckResult(2, "II"));
+    it("should parse 3 to III", parseAndCheckResult(3, "III"));
+    it("should parse 20 to XX", parseAndCheckResult(20, "XX"));
+    it("should parse 3000 to MMM", parseAndCheckResult(3000, "MMM"));
+});
+
+describe("Should parse arabic numbers to multiple digit Roman Numerals: ", function() {
+    it("should parse 3 to III", parseAndCheckResult(3, "III"));
+    it("should parse 8 to VIII", parseAndCheckResult(8, "VIII"));
+    it("should parse 550 to DL", parseAndCheckResult(550, "DL"));
+    it("should parse 552 to DLII", parseAndCheckResult(552, "DLII"));
+    it("should parse 256 to CCLVI", parseAndCheckResult(256, "CCLVI"));
+    it("should parse 1105 to MCV", parseAndCheckResult(1105, "MCV"));
+});
+
+describe("Should parse arabic numbers requiring subtration numerals: ", function() {
+    it("should parse 4 to IV", parseAndCheckResult(4, "IV"));
+    it("should parse 9 to IX", parseAndCheckResult(9, "IX"));
+    it("should parse 40 to XL", parseAndCheckResult(40, "XL"));
+    it("should parse 1004 to MIV", parseAndCheckResult(1004, "MIV"));
+    it("should parse 34 to XXXIV", parseAndCheckResult(34, "XXXIV"));
+    it("should parse 124 to CXXIV", parseAndCheckResult(124, "CXXIV"));
+    it("should parse 290 to CCXC", parseAndCheckResult(290, "CCXC"));
+    it("should parse 1969 to MCMLXIX", parseAndCheckResult(1969, "MCMLXIX"))
 });
