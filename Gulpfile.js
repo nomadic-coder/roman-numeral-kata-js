9a67b84f311bc0f9d6afe6573fa858ec3ffe4ab6
Satish Kalia <satish.kalia@gmail.com>
2016-07-21 12:28:31 +0100

    tidying up

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,17 +7,3 @@ gulp.task('compress', function () {
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
-
-
-// var gulp  = require('gulp'),
-//     gutil = require('gulp-util'),
-//     install = require('gulp-install');
-//
-// gulp.task('log', function() {
-//     return gutil.log('gulp is running');
-// });
-//
-// gulp.task('npminstall', function() {
-//     return gulp.src(['./package.json'])
-//         .pipe(install());
-// });
