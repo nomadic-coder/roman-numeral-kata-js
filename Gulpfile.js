f9c34e516964836c0f5f81811d99b81a63b533d9
Satish Kalia <satish.kalia@gmail.com>
2016-07-21 12:45:11 +0100

    added gulp clean task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,15 @@
 
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
 
 gulp.task('compress', function () {
     return gulp.src('app/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+	return gulp.src(['dist','node_modules'], {read:false})
+		.pipe(clean());
 });
